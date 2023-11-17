cf586e99c819fc406e5b990b42006b8e52161664
blade <yiming.liao@cuw.edu>
2023-11-17 11:37:16 -0600

    fix code error in while loop
    1. fix the condition in while loop
    2. improve the performance

--- src/main/java/drive/ShuntingYard.java
+++ src/main/java/drive/ShuntingYard.java
@@ -53,20 +53,27 @@ public class ShuntingYard {
         String s = "*+/-";
         int x;
         int y;
-        while(ReversePolish.isEmpty()) {
+        while(!ReversePolish.isEmpty()) {
         	temp = ReversePolish.dequeue().Data;
         	if(temp.indexOf(s) >= 0) {
         		x = Integer.parseInt(tempstack.pop().Data);
         		y = Integer.parseInt(tempstack.pop().Data);
-        		if(temp.charAt(0) == s.charAt(0)) {
-        			x = y * x;
-        		} else if (temp.charAt(0) == s.charAt(1)) {
-        			x = y + x;
-        		} else if (temp.charAt(0) == s.charAt(2)) {
-        			x = y / x;
-        		} else {
-        			x = y - x;
-        		}
+                switch (temp.charAt(0)) {
+                    case '*':
+                        x = y * x;
+                        break;
+                    case '/':
+                        x = y / x;
+                        break;
+                    case '+':
+                        x = y + x;
+                        break;
+                    case '-':
+                        x = y - x;
+                        break;
+                    default:
+                        break;
+                }
         		tempstack.push(Integer.toString(x));
         	} else {
         		tempstack.push(temp);
